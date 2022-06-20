8565285c8cde8d86964c376dd9826819b27440d4
Fabricio Silva <fabriciosilvalp@outlook.com>
2022-06-19 21:04:43 -0300

    Initial Commit

---
+++ InstantHireDeveloper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InstantHireDeveloper
+//
+//  Created by Fabricio Silva on 19/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InstantHireDeveloper/InstantHireDeveloperApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstantHireDeveloperApp.swift
+//  InstantHireDeveloper
+//
+//  Created by Fabricio Silva on 19/06/22.
+//
+
+import SwiftUI
+
+@main
+struct InstantHireDeveloperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
